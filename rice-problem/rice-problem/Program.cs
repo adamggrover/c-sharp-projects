1751b9d11057f2cf84151d0f845f86f938b89fdc
adamggrover <93373074+adamggrover@users.noreply.github.com>
2022-11-06 16:08:53 +0000

    Create folder of existing projects

---
+++ rice-problem/rice-problem/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int iSquareNum = 1;
+double dGrainNum = 1;
+double dGrainTotal = 0;
+
+
+do
+{
+    dGrainTotal += dGrainNum;
+
+    dGrainNum = dGrainNum * 2;
+
+    Console.WriteLine(dGrainTotal);
+
+    iSquareNum++;
+
+    
+
+}
+while (iSquareNum < 65);
+
+Console.Read();
